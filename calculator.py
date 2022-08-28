6c73de8e4f1eba1e26cafefd7c80daf28c5a3b2d
gdjagdale <ganeshjagdale30@gmail.com>
2022-08-28 14:31:54 +0530

    Feature division

--- calculator.py
+++ calculator.py
@@ -7,7 +7,11 @@ def add(a,b):
 print(f"Addition of {a} and {b} is {add(10,20)}")
 
 def sub(a,b):
-    return a+b
+    return a-b
 
 print(f"Substraction of {a} and {b} is {sub(10,20)}")
 
+def div(a,b):
+    return a/b
+
+print(f"Division of {a} and {b} is {div(a,b)}")
